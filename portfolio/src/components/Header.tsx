5df202249d2e7ac6935a68588c9a3eb422e933c8
DimaShunkoA <127039068+DimaShunkoA@users.noreply.github.com>
2023-03-05 18:37:35 +0300

    Add files via upload

---
+++ portfolio/src/components/Header.tsx
@@ -0,0 +1,20 @@
+// @flow
+import * as React from 'react';
+import Logo from '../assets/logo.svg'
+
+export const Header = () => {
+    return (
+        <header className='py-8'>
+            <div className='container mx-auto'>
+                <div className='flex justify-between items-center'>
+                    <a href='#'>
+                        <img src={Logo} alt=''/>
+                    </a>
+                    <button className='btn btn-sm'>
+                        Work with me
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+};
